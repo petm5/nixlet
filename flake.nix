f85fafc1fd75f4c3529cd681fe6a31506cf5ca54
Peter Marshall <petms@proton.me>
2023-12-05 11:02:43 -0500

    First working build

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A minimal server image";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+  outputs = { self, nixpkgs }: rec {
+    nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
+        "${nixpkgs}/nixos/modules/profiles/image-based-appliance.nix"
+        "${nixpkgs}/nixos/modules/profiles/headless.nix"
+        ({pkgs, lib, ...}:
+        {
+          isoImage = {
+            volumeID = lib.mkForce "nixos";
+            isoName = lib.mkForce "nixos.iso";
+            squashfsCompression = "zstd -Xcompression-level 6";
+            makeEfiBootable = true;
+            makeUsbBootable = true;
+            makeBiosBootable = false;
+            edition = "minimal";
+          };
+          boot = {
+            loader.timeout = lib.mkForce 0;
+            initrd.systemd.enable = lib.mkForce false;
+          };
+        })
+        ./configuration.nix
+      ];
+    };
+    images.iso = self.nixosConfigurations.iso.config.system.build.isoImage;
+    packages.x86_64-linux.default = self.images.iso;
+  };
+}
