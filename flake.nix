39a4381bc82a3bc7f81f4b13d2de67cd04918eb3
Peter Marshall <petms@proton.me>
2023-12-08 10:16:46 -0500

    Convert into a reusable library, remove unrelated ISO generator

--- flake.nix
+++ flake.nix
@@ -1,27 +1,25 @@
 {
-  description = "Build OTA-updatable disk images for servers";
+  description = "Build OTA-updatable disk images for appliances";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/23.11";
   };
-  outputs = { self, nixpkgs }: rec {
-    nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./iso-image.nix
-        ./configuration.nix
-      ];
-    };
-    nixosConfigurations.img = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./efi-ab-image.nix
-        ./configuration.nix
-      ];
-    };
-    images.iso = self.nixosConfigurations.iso.config.system.build.isoImage;
-    images.efi-ab = self.nixosConfigurations.img.config.system.build.diskImage;
-    components.squashfs = self.nixosConfigurations.img.config.system.build.squashfsStore;
-    components.uki = self.nixosConfigurations.img.config.system.build.uki;
-    packages.x86_64-linux.default = self.images.iso;
+  outputs = { self, nixpkgs }: {
+    nixosGenerate = {
+      pkgs ? null,
+      lib ? nixpkgs.lib,
+      nixosSystem ? nixpkgs.lib.nixosSystem,
+      system ? null,
+      modules ? []
+    }: let
+      formatModule = ./efi-ab-image.nix;
+      image = nixosSystem {
+        inherit pkgs system lib;
+        modules = [
+          formatModule
+        ]
+        ++ modules;
+      };
+    in
+      image.config.system.build.diskImage;
   };
 }
