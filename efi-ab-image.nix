345c0076110159c650c09980b5b93f5e70a2d1b0
Peter Marshall <petms@proton.me>
2023-12-28 13:47:40 -0500

    Set ChromeOS kernel partition flags properly

--- efi-ab-image.nix
+++ efi-ab-image.nix
@@ -172,7 +172,7 @@ in
             Label = "KERN-A";
             SizeMinBytes = "16M";
             SizeMaxBytes = "16M";
-            Flags = "0b001000000000000000000000000000000000000000000000000000000000000000";
+            Flags = "0b0000000100000001000000000000000000000000000000000000000000000000";
             CopyBlocks = "${config.boot.loader.depthcharge.kernelPart}";
           };
         };
