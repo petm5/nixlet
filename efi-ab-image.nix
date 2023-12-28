42dd8cf1812943980aecd77e11417e31716d9490
Peter Marshall <petms@proton.me>
2023-12-28 12:39:32 -0500

    Play around with partition flags

--- efi-ab-image.nix
+++ efi-ab-image.nix
@@ -196,7 +196,7 @@ in
             Type = "root-${arch}";
             Label = "${version}";
             CopyBlocks = "${config.system.build.squashfsStore}";
-            ReadOnly = true;
+            Flags = "0xFFFFFFFFFFFFFFFF";
           };
         };
       };
