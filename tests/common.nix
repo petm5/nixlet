210c0d6d0d3c8bff5a99c35c0da118b64e8e7e2e
Peter Marshall <petms@proton.me>
2024-09-04 10:53:55 -0400

    Separate common test functions

---
+++ tests/common.nix
@@ -0,0 +1,110 @@
+{ self, lib, pkgs, ... }:
+
+with import (pkgs.path + "/nixos/lib/testing-python.nix") { inherit pkgs; inherit (pkgs.hostPlatform) system; };
+
+let
+
+  nixos-lib = import (pkgs.path + "/nixos/lib") {};
+  qemu-common = import (pkgs.path + "/nixos/lib/qemu-common.nix") { inherit lib pkgs; };
+
+in rec {
+
+  makeSystem = extraConfig:
+  (import (pkgs.path + "/nixos/lib/eval-config.nix")) {
+    inherit pkgs lib;
+    system = null;
+    modules = [
+      {
+        nixpkgs.hostPlatform = pkgs.hostPlatform;
+      }
+      {
+        users.allowNoPasswordLogin = true;
+        system.stateVersion = lib.versions.majorMinor lib.version;
+        system.image.id = lib.mkDefault "test";
+        networking.hosts."10.0.2.1" = [ "server.test" ];
+      }
+      {
+        boot.kernelParams = [ "console=ttyS0,115200n8" "systemd.journald.forward_to_console=1" ];
+        image.repart.mkfsOptions.squashfs = lib.mkForce [ "-comp zstd" "-Xcompression-level 6" "-b 256K" ];
+        boot.initrd.compressor = lib.mkForce "zstd";
+        boot.initrd.compressorArgs = lib.mkForce [ "-8" ];
+      }
+      (pkgs.path + "/nixos/modules/testing/test-instrumentation.nix")
+      self.nixosModules.server
+      self.nixosModules.image
+      extraConfig
+    ];
+  };
+
+  makeImage = extraConfig: let
+    system = makeSystem extraConfig;
+  in "${system.config.system.build.image}/${system.config.image.repart.imageFile}";
+
+  makeUpdatePackage = extraConfig: let
+    system = makeSystem extraConfig;
+  in "${system.config.system.build.updatePackage}";
+
+  makeImageTest = { name, image, script, httpRoot ? null }: let
+    qemu = qemu-common.qemuBinary pkgs.qemu_test;
+    flags = [
+      "-m" "512M"
+      "-drive" "if=pflash,format=raw,unit=0,readonly=on,file=${pkgs.OVMF.firmware}"
+      "-drive" "if=pflash,format=raw,unit=1,readonly=on,file=${pkgs.OVMF.variables}"
+      "-drive" "if=virtio,file=${mutableImage}"
+      "-chardev" "socket,id=chrtpm,path=${tpmFolder}/swtpm-sock"
+      "-tpmdev" "emulator,id=tpm0,chardev=chrtpm"
+      "-device" "tpm-tis,tpmdev=tpm0"
+      "-netdev" ("'user,id=net0" + (lib.optionalString (httpRoot != null) ",guestfwd=tcp:10.0.2.1:80-cmd:${pkgs.micro-httpd}/bin/micro_httpd ${httpRoot}") + "'")
+      "-device" "virtio-net-pci,netdev=net0"
+    ];
+    flagsStr = lib.concatStringsSep " " flags;
+    startCommand = "${qemu} ${flagsStr}";
+    mutableImage = "/tmp/linked-image.qcow2";
+    tpmFolder = "/tmp/emulated_tpm";
+    indentLines = str: lib.concatLines (map (s: "  " + s) (lib.splitString "\n" str));
+  in makeTest {
+    inherit name;
+    nodes = { };
+    testScript = ''
+      import os
+      import subprocess
+
+      subprocess.check_call(
+          [
+              "qemu-img",
+              "create",
+              "-f",
+              "qcow2",
+              "-F",
+              "raw",
+              "-b",
+              "${image}",
+              "${mutableImage}",
+          ]
+      )
+      subprocess.check_call(["qemu-img", "resize", "${mutableImage}", "4G"])
+
+      os.mkdir("${tpmFolder}")
+      os.mkdir("${tpmFolder}/swtpm")
+
+      def start_tpm():
+        subprocess.Popen(
+            [
+                "${pkgs.swtpm}/bin/swtpm",
+                "socket",
+                "--tpmstate", "dir=${tpmFolder}/swtpm",
+                "--ctrl", "type=unixio,path=${tpmFolder}/swtpm-sock",
+                "--tpm2"
+            ]
+        )
+
+      machine = create_machine("${startCommand}")
+
+      try:
+    '' + indentLines script + ''
+      finally:
+        machine.shutdown()
+    '';
+  };
+
+}
--- tests/system-update.nix
+++ tests/system-update.nix
@@ -1,144 +1,37 @@
-{ pkgs, self }:
+{ pkgs, self }: let
 
-with import (pkgs.path + "/nixos/lib/testing-python.nix") { inherit pkgs; inherit (pkgs.hostPlatform) system; };
-
-let
   lib = pkgs.lib;
-  nixos-lib = import (pkgs.path + "/nixos/lib") {};
-  qemu-common = import (pkgs.path + "/nixos/lib/qemu-common.nix") { inherit lib pkgs; };
+  test-common = import ./common.nix { inherit self lib pkgs; };
 
-  makeSystem = extraConfig:
-  (import (pkgs.path + "/nixos/lib/eval-config.nix")) {
-    inherit pkgs lib;
-    system = null;
-    modules = [
-      {
-        nixpkgs.hostPlatform = pkgs.hostPlatform;
-      }
-      {
-        users.allowNoPasswordLogin = true;
-        system.stateVersion = lib.versions.majorMinor lib.version;
-        system.image.id = "test";
-        system.image.updates.url = "http://10.0.2.1/";
-      }
-      {
-        boot.kernelParams = [ "console=ttyS0,115200n8" "systemd.journald.forward_to_console=1" ];
-        image.repart.mkfsOptions.squashfs = lib.mkForce [ "-comp zstd" "-Xcompression-level 6" "-b 256K" ];
-        boot.initrd.compressor = lib.mkForce "zstd";
-        boot.initrd.compressorArgs = lib.mkForce [ "-8" ];
-      }
-      (pkgs.path + "/nixos/modules/testing/test-instrumentation.nix")
-      self.nixosModules.server
-      self.nixosModules.image
-      extraConfig
-    ];
-  };
-
-  testSystem = makeSystem {
+  initialImage = test-common.makeImage {
     system.image.version = "1";
+    system.image.updates.url = "http://server.test/";
   };
 
-  updateSystem = makeSystem {
+  updatePackage = test-common.makeUpdatePackage {
     system.image.version = "2";
   };
 
-  systemd = testSystem.config.systemd.package;
-
-  diskImage = "${testSystem.config.system.build.image}/${testSystem.config.image.repart.imageFile}";
-  updateImage = "${updateSystem.config.system.build.image}/${updateSystem.config.image.repart.imageFile}";
-
-  mutableImage = "/tmp/linked-image.qcow2";
-  tpmFolder = "/tmp/emulated_tpm";
-
-  httpRoot = updateSystem.config.system.build.updatePackage;
-
-  lighttpdConfig = pkgs.writeText "lighttpd.conf" ''
-    server.document-root = "${httpRoot}"
-    server.port = 8989
-  '';
-
-  startCommand = let
-    qemu = qemu-common.qemuBinary pkgs.qemu_test;
-    flags = [
-      "-m" "512M"
-      "-drive" "if=pflash,format=raw,unit=0,readonly=on,file=${pkgs.OVMF.firmware}"
-      "-drive" "if=pflash,format=raw,unit=1,readonly=on,file=${pkgs.OVMF.variables}"
-      "-drive" "if=virtio,file=${mutableImage}"
-      "-chardev" "socket,id=chrtpm,path=${tpmFolder}/swtpm-sock"
-      "-tpmdev" "emulator,id=tpm0,chardev=chrtpm"
-      "-device" "tpm-tis,tpmdev=tpm0"
-      "-netdev" "user,id=net0,guestfwd=tcp:10.0.2.1:80-tcp:127.0.0.1:8989"
-      "-device" "virtio-net-pci,netdev=net0"
-    ];
-    flagsStr = lib.concatStringsSep " " flags;
-  in "${qemu} ${flagsStr}";
-in makeTest {
+in test-common.makeImageTest {
   name = "system-update";
-  nodes = { };
-  testScript = ''
-    import os
-    import subprocess
+  image = initialImage;
+  httpRoot = updatePackage;
+  script = ''
+    start_tpm()
+    machine.start()
 
-    subprocess.check_call(["cat", "${httpRoot}/SHA256SUMS"])
+    machine.wait_for_unit("multi-user.target")
+    machine.wait_for_unit("network.target")
 
-    subprocess.check_call(
-        [
-            "qemu-img",
-            "create",
-            "-f",
-            "qcow2",
-            "-F",
-            "raw",
-            "-b",
-            "${diskImage}",
-            "${mutableImage}",
-        ]
-    )
-    subprocess.check_call(["qemu-img", "resize", "${mutableImage}", "4G"])
+    machine.succeed("/run/current-system/sw/lib/systemd/systemd-sysupdate update")
 
-    os.mkdir("${tpmFolder}")
-    os.mkdir("${tpmFolder}/swtpm")
-
-    def start_tpm():
-      subprocess.Popen(
-          [
-              "${pkgs.swtpm}/bin/swtpm",
-              "socket",
-              "--tpmstate", "dir=${tpmFolder}/swtpm",
-              "--ctrl", "type=unixio,path=${tpmFolder}/swtpm-sock",
-              "--tpm2"
-          ]
-      )
-
-    subprocess.Popen(
-        [
-            "${pkgs.lighttpd}/bin/lighttpd",
-            "-f" "${lighttpdConfig}",
-            "-D"
-        ]
-    )
-
-    machine = create_machine("${startCommand}")
+    machine.shutdown()
 
     start_tpm()
     machine.start()
 
-    try:
-      machine.wait_for_unit("multi-user.target")
-      machine.wait_for_unit("network.target")
+    machine.wait_for_unit("multi-user.target")
 
-      machine.succeed("${systemd}/lib/systemd/systemd-sysupdate check-new")
-      machine.succeed("${systemd}/lib/systemd/systemd-sysupdate update")
-
-      machine.shutdown()
-
-      start_tpm()
-      machine.start()
-
-      machine.wait_for_unit("multi-user.target")
-
-      machine.succeed('. /etc/os-release; [ "$IMAGE_VERSION" == "${updateSystem.config.system.image.version}" ]')
-    finally:
-      machine.shutdown()
+    machine.succeed('. /etc/os-release; [ "$IMAGE_VERSION" == "2" ]')
   '';
 }
