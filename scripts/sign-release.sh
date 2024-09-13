ea65e77f19a25202a70a8a699c1d7ec1c62cbb11
Peter Marshall <petms@proton.me>
2024-09-13 13:08:36 -0400

    Fixup actions

--- scripts/sign-release.sh
+++ scripts/sign-release.sh
@@ -6,8 +6,8 @@ set -eux
 mkdir signed
 cp -L result/* signed/
 
-sudo losetup -P /dev/loop0 signed/*.img
-sudo mount /dev/loop0p1 /mnt -t vfat
+loopdev=$(sudo losetup -P -f signed/*.img)
+sudo mount "${loopdev}p1" /mnt -t vfat
 
 sudo find signed/ /mnt/ -name "*.efi" -type f -exec sbsign --key <(echo "$DB_KEY") --cert <(echo "$DB_CRT") --output {} {} \;
 
@@ -15,4 +15,4 @@ sudo mkdir -p /mnt/loader/keys/nixlet
 sudo cp keys/*.auth /mnt/loader/keys/nixlet/
 
 sudo umount /mnt
-sudo losetup -d /dev/loop0
+sudo losetup -d "$loopdev"
