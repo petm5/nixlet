0556965ac4101afe715bfd7d55711d9a58c58c29
Peter Marshall <petms@proton.me>
2025-05-08 14:14:32 -0400

    Bump version

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       system = "x86_64-linux";
     };
     updateUrl = "https://github.com/petm5/nixlet/releases/latest/download";
-    releaseVersion = "0.1.6";
+    releaseVersion = "0.2.0";
     baseConfig = [
       ./modules/profiles/minimal.nix
       ./modules/profiles/image-based.nix
