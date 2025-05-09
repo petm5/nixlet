516578e71385a232611398bfc79023115b13db3e
Peter Marshall <petms@proton.me>
2025-05-09 15:02:32 -0400

    Remove our systemd override

--- modules/profiles/minimal.nix
+++ modules/profiles/minimal.nix
@@ -22,15 +22,6 @@
   # Modules must be loaded by initrd
   boot.initrd.kernelModules = config.boot.kernelModules;
 
-  systemd.package = pkgs.systemd.overrideAttrs {
-    src = pkgs.fetchFromGitHub {
-      owner = "petm5";
-      repo = "systemd";
-      rev = "c70d5474185d1bc49bdc1a5a296694ae7194c08d";
-      hash = "sha256-kXySBrV/lGJD34va2oSZ67B+f+IUav1Vv9UvwLe3Z0g=";
-    };
-  };
-
   boot.kernelModules = [
     # Required for systemd SMBIOS credential import
     "dmi_sysfs"
